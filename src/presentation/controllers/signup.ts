f6754612eec74f3b0683d22c4625cd096a6ebcee
Narval1 <matheushsm51@gmail.com>
2023-03-22 14:51:55 -0300

    feat: ensure SignupController interface implements the corrects interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,21 @@
+import { type HttpRequest, type HttpResponse } from '../protocols/http'
+
 export class SignupController {
-  handle (httpRequest: any): any {
-    return { statusCode: 400 }
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const { body } = httpRequest
+    const { name, email } = body
+    if (name === undefined) {
+      return {
+        body: new Error('Missing param: name.'),
+        statusCode: 400
+      }
+    }
+    if (email === undefined) {
+      return {
+        body: new Error('Missing param: email.'),
+        statusCode: 400
+      }
+    }
+    return { statusCode: 400, body: '' }
   }
 }
---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,9 @@
+// obrigo as clases a possuirem um padrao de recebimento e envio, impedindo erros de compilacao
+export interface HttpResponse {
+  statusCode: number
+  body: any
+}
+// clases do tipo get nao precisam de um body necessariamente, podendo ser null
+export interface HttpRequest {
+  body?: any
+}
