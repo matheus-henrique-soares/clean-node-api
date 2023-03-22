9ad2e1e16e04ee6b4564f6760361a050e4c4ee90
Narval1 <matheushsm51@gmail.com>
2023-03-22 14:52:12 -0300

    test: ensure SignupController interface implements the corrects interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,5 +13,20 @@ describe('SIgnup Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name.'))
+  })
+  test('Should return 400 if no email is provided', () => {
+    // system under test
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_email@email.com',
+        passwordConfirmation: 'any_email@email.com'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email.'))
   })
 })
