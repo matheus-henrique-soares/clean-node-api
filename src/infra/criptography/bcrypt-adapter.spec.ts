5e1c870680316706f9cc9a7944b7ddb940f7c9e4
Narval1 <matheushsm51@gmail.com>
2023-04-15 11:28:13 -0300

    feat: ensure a lot

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,14 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => {
+  return {
+    async hash (value: string): Promise<string> {
+      return await new Promise((resolve) => { resolve('any_hash') })
+    }
+  }
+})
+
 interface SutTypes {
   salt: number
   sut: BcryptAdapter
@@ -21,5 +29,16 @@ describe('Bcrypt adapter.', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('Should return a hash on success.', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('any_hash')
+  })
+  test('Should throw if bcrypt throws.', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
