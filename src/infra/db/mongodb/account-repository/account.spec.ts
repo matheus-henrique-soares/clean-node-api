63027a01ec1cdd9ed2536c7a8dece564a560cc44
Narval1 <matheushsm51@gmail.com>
2023-04-17 14:12:09 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -13,6 +13,10 @@ describe('Account mongo repository.', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   test('Should return an account on success.', async () => {
     const sut = makeSut()
