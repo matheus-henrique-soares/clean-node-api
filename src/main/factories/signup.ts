42165bda11e0546e6b5070a70c629a5d3e4459a5
Narval1 <matheushsm51@gmail.com>
2023-04-19 15:45:23 -0300

    feat: add signup facctorie

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignupController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../presentation/utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignupController = (): SignupController => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  return new SignupController(emailValidatorAdapter, dbbAddAccount)
+}
