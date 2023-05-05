b7440db8ae3e1744cd2cdd8a2acbe5fa0b241009
Narval1 <matheushsm51@gmail.com>
2023-05-05 10:28:08 -0300

    feat: ensure correct integration with EmailValidator

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,15 +1,31 @@
-import { MissingParamError } from '../../errors'
-import { badRequest, ok } from '../../helpers/htpp-helper'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/htpp-helper'
 import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols'
+import { type EmailValidator } from '../../protocols/email-validator'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
-    for (const param of requiredFields) {
-      if (httpRequest.body[param] === undefined) {
-        return badRequest(new MissingParamError(param))
+    try {
+      const requiredFields = ['email', 'password']
+      for (const param of requiredFields) {
+        if (httpRequest.body[param] === undefined) {
+          return badRequest(new MissingParamError(param))
+        }
       }
+      const { email } = httpRequest.body
+      const emailIsValid = this.emailValidator.isValid(email)
+      if (emailIsValid === false) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return ok(httpRequest.body)
+    } catch (error) {
+      return serverError(error)
     }
-    return ok(httpRequest.body)
   }
 }
