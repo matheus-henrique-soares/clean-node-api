a9a01db81c3257870589ef908fcb3a9909f74601
Narval1 <matheushsm51@gmail.com>
2023-05-18 13:21:50 -0300

    test: ensure DbAUthewntication returns null if hashCOmparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -89,4 +89,10 @@ describe('DBAuhtentication usecase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+  test('Should return null if HashComparer returns false.', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => { resolve(false) }))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
