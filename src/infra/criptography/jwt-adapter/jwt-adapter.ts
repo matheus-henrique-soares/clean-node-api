79f121beb09a3b01903a65ed2d9ce5622d68052d
Narval1 <matheushsm51@gmail.com>
2023-05-25 09:35:08 -0300

    feat: ensure jwt adapter returns a token when sign succeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -9,7 +9,7 @@ export class JwtAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secret)
-    return await new Promise(resolve => { resolve(null) })
+    const accessToken = jwt.sign({ id: value }, this.secret)
+    return await new Promise(resolve => { resolve(accessToken) })
   }
 }
