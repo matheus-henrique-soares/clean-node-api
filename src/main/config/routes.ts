7c1dd3aad4ced448a23d6fa2f0a96e14c7b89230
Narval1 <matheushsm51@gmail.com>
2023-06-12 14:12:07 -0300

    refactor: change routes to use filesytem instead of fg

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,14 @@
 import { type Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-    const route = (await import(`../../../${file}`)).default
-    route(router)
+  readdirSync(path.join(`${__dirname}`, '../', 'routes')).map(async file => {
+    if (!file.includes('.test.')) {
+      await import(`../routes/${file}`)
+        .then((obj) => { obj.default(router) })
+    }
   })
 }
