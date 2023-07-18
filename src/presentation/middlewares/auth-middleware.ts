96494da009cd609a3360623c975fe0dc544122c5
Narval1 <matheushsm51@gmail.com>
2023-07-18 13:21:08 -0300

    feat: ensure authMiddleware if LoadAccountByToken succedss

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { type LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden } from '../helpers/http/htpp-helper'
+import { forbidden, ok } from '../helpers/http/htpp-helper'
 import { type Middleware, type HttpRequest, type HttpResponse } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -9,7 +9,8 @@ export class AuthMiddleware implements Middleware {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token']
     if (accessToken) {
-      await this.loadAccountByToken.load(accessToken)
+      const account = await this.loadAccountByToken.load(accessToken)
+      if (account) return ok({ accountId: account.id })
     }
     return forbidden(new AccessDeniedError())
   }
