d4e8a089004d3462dd4cb8bc191efa8ea9dbd8a5
Narval1 <matheushsm51@gmail.com>
2023-08-02 09:43:41 -0300

    feat: ensure LoadSurveysController returns 204 if LoadSurveys returns am empty array

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,5 +1,5 @@
 import { type LoadSurveys, type Controller, type HttpRequest, type HttpResponse } from './load-surveys-controller-protocols'
-import { ok, serverError } from '../../../helpers/http/htpp-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/htpp-helper'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
@@ -7,6 +7,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
+      if (surveys.length === 0) return noContent()
       return ok(surveys)
     } catch (error) {
       return serverError(error)
